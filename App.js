54bc5b99eaf9a5723a3e5bb97bf09860e7ee1dbb
Mitch <mitchell.hayen@gmail.com>
2022-02-14 18:47:05 -0600

    Mitchell changed login screen title

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ const App = () => {
                 <Stack.Screen
                     name="Login"
                     component={LoginScreen}
-                    options={{title:'This is photo boom'}}
+                    options={{title:'Mitchell test commit 1'}}
                 />
                 <Stack.Screen
                     name={"Landing"}
